00e064bddfd5de6be6b4bffda7b21f9ac5a5b3da
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2024-03-08 08:22:51 -0800

    first commit
    author: dangkyle64
    date: Mar 8 2024 0822

---
+++ lexer.py
@@ -0,0 +1,28 @@
+'''
+
+def lexer()
+    returns token, token_value = instance of token (lexeme)
+
+implement FSA for tokens
+    identifier and integers, others can be written extra
+
+write REs
+draw FSA for above required tokens (identifier and integer)
+    put this into document called FSA_mydesign.doc
+
+print result into 2 columns, one for token and one for lexemes
+    save result into output.txt
+
+    ex.
+
+    token       lexeme
+    keyword     while
+    separator   ()
+    identifier  t
+    real        22.00
+
+readme should include documentation and how to setup program
+
+resulting files should have input_scode.txt, design_file, program_file, output_file, readme_file
+
+'''
