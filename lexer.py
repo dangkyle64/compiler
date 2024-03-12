402edd498f135dd4f20ff3fcd0b628e722b068e1
dangkyle64 <156500580+dangkyle64@users.noreply.github.com>
2024-03-12 06:04:20 -0700

    split files for organization
    author: dangkyle64
    date: Mar 12 2024 0602
    
    split the header notes into its own file to reduce excess informational clutter on file
    
    Moved FSA design into its own text file for both easier access and to reduce the clutter on the main lexer file
    
    Removing excess test code from past iterations of the compiler to clean up before future additions

--- lexer.py
+++ lexer.py
@@ -1,70 +1,3 @@
-'''
-def lexer()
-    returns token, token_value = instance of token (lexeme)
-
-implement FSA for tokens
-    identifier and integers, others can be written extra
-
-write REs
-draw FSA for above required tokens (identifier and integer)
-    put this into document called FSA_mydesign.doc
-
-print result into 2 columns, one for token and one for lexemes
-    save result into output.txt
-
-    ex.
-
-    token       lexeme
-    keyword     while
-    separator   ()
-    identifier  t
-    real        22.00
-
-readme should include documentation and how to setup program
-
-resulting files should have input_scode.txt, design_file, program_file, output_file, readme_file
-
-States:
-    S: Inital State
-    A: Accepting State 
-
-Alphabet
-    {'variable_name',_underscored_word, myFunction, MOD_NAME, cat123, __init__, digits}
-
-Transitions
-    From State start_state:
-        On input [a-zA-Z_]: Transition to identifier_start
-        On input digits: Transition to integer_start
-        Otherwise: Transition to invalid_input
-
-    From State identifier_start:
-        On input [a-zA-Z0-9_]: Transition to identifier_continue
-        Otherwise: Transition to invalid_input
-
-    From State identifier_continue:
-        On input [a-zA-Z0-9_]: Transition to identifier_continue
-        When finished: Transition to accepted_state
-        Otherwise: Transition to invalid_input
-
-    From State integer_start:
-        On input [0-9]: Transition to integer_continue
-        Otherwise: Transition to invalid_input
-
-    From State integer_continue:
-        On input [0-9]: Transition to integer_continue
-        When finished: Transition to accepted_state
-        Otherwise: Transition to invalid_input
-
-    From State invalid_input
-        any input: Transition to invalid
-
-    From State invalid
-        any input: return ERROR_TOKEN
-
-    From State accepted_state:
-        Accepting State (include different accepting states for each token)
-
-'''
 #create dictionary to define the different transitions 
 transitions = {
     "current_state": {
@@ -105,7 +38,7 @@ transitions = {
 def lexer():
 
     #setting state at beginning
-    state = current_state
+    state = 'current_state'
 
     #inputs would go here (future file inputs to test inputs would also go here)
     #potentially make a separate function that would return into the lexer specifically
@@ -148,8 +81,6 @@ def lexer():
 
     return 0
 
-
-
 #find next transition state using dictionary and in puts
 def next_state(current_state, input_char):
     input_type = get_input_type(input_char)
@@ -177,17 +108,4 @@ def get_input_type(input_char):
     else:
         return "other"
 
-#setting state at beginning
-current_state = "current_state"
-
-#inputs being put in one by one into input char
-input_char = "52"
-
-#get the specific type to compare with the transition table
-#input_type = get_input_type(input_char)
-#print ("Input type", input_type)
-
-#find where the next state is after transition 
-#next_state = next_state(current_state, input_char)
-#print ("next_state:", next_state)
 lexer()
